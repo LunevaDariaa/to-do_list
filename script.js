ea15a2b34c0356aa3e5d31f95d280ffd85d13dea
Daria Luneva <daria.safri@gmail.com>
2024-02-20 13:14:51 -0500

    Created notepad and started scripting
    Styles for notes and started js logic

--- script.js
+++ script.js
@@ -2,12 +2,36 @@
 const addActBtn = document.querySelector(".add-activity-btn");
 const modalWindow = document.querySelector(".add_task_page");
 const closeModalBtn = document.querySelector(".add_task_close_btn");
+const addNoteBtn = document.querySelector(".add_note_btn");
+const overlay = document.querySelector(".overlay");
+const noteTextarea = document.querySelector("#add_note_input");
+
+const noteContainer = document.querySelector(".notes");
 
 // EventListeners
 const openModal = addActBtn.addEventListener("click", function () {
   modalWindow.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 });
 
 const closeModal = closeModalBtn.addEventListener("click", function () {
   modalWindow.classList.add("hidden");
+  overlay.classList.add("hidden");
 });
+
+const addNote = addNoteBtn.addEventListener("click", function () {
+  newNote = noteTextarea.value;
+  noteCreation(newNote);
+});
+
+const noteCreation = function (note) {
+  const text = `<div id="note">
+  <div class="note_text">
+   ${note}
+   div class="note_btns">
+   <button class="note_edit_btn">🖋</button>
+   <button class="note_dlt_btn">🗑</button>
+ </div>
+ </div>`;
+  noteContainer.insertAdjacentHTML("afterbegin", text);
+};
