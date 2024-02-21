77aa9af13f8829726d17479dcac961ceffef5175
Daria Luneva <145506754+LunevaDariaa@users.noreply.github.com>
2024-02-20 23:00:57 -0500

    restructure script into encrypted code
    added logic for types of activities

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 //Selectors
 const addActBtn = document.querySelector(".add-activity-btn");
 const modalWindow = document.querySelector(".add_task_page");
@@ -5,33 +6,66 @@ const closeModalBtn = document.querySelector(".add_task_close_btn");
 const addNoteBtn = document.querySelector(".add_note_btn");
 const overlay = document.querySelector(".overlay");
 const noteTextarea = document.querySelector("#add_note_input");
-
+const addTaskSidebar = document.querySelector(".add_task_sidebar");
 const noteContainer = document.querySelector(".notes");
+//Selectors for types of activity:
+const addNote = document.querySelector(".add_note_content");
+const addTask = document.querySelector(".add_task_content");
+const addProject = document.querySelector(".add_project_content");
+//All data
 
-// EventListeners
-const openModal = addActBtn.addEventListener("click", function () {
-  modalWindow.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+class App {
+  #notes = [];
+
+  constructor() {
+    addActBtn.addEventListener("click", this._openModal.bind(this));
+    closeModalBtn.addEventListener("click", this._closeModal.bind(this));
+    // addNoteBtn.addEventListener("click", this._addNote.bind(this));
+  }
+
+  //methods
+  _noteCreation(note) {
+    const text = `<div id="note">
+    <div class="note_text">
+     ${note}
+     <div class="note_btns">
+     <button class="note_edit_btn">🖋</button>
+     <button class="note_dlt_btn">🗑</button>
+   </div>
+   </div>`;
+    noteContainer.insertAdjacentHTML("afterbegin", text);
+  }
+
+  _defineType(e) {
+    e.preventDefault();
+    if (e.target.classList.contains("add_task_task")) {
+      addNote.classList.add("hidden");
+      addTask.classList.remove("hidden");
+    }
+    if (e.target.classList.contains("add_task_project")) {
+      console.log("project");
+    }
+    if (e.target.classList.contains("add_task_note")) {
+      addNote.classList.remove("hidden");
+      addTask.classList.add("hidden");
+    }
+  }
+  _openModal() {
+    modalWindow.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    addTaskSidebar.addEventListener("click", this._defineType);
+  }
+
+  _closeModal() {
+    modalWindow.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+
+  _addNote() {
+    const newNote = noteTextarea.value;
+    this._noteCreation(newNote);
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const appInstance = new App();
 });
-
-const closeModal = closeModalBtn.addEventListener("click", function () {
-  modalWindow.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
-
-const addNote = addNoteBtn.addEventListener("click", function () {
-  newNote = noteTextarea.value;
-  noteCreation(newNote);
-});
-
-const noteCreation = function (note) {
-  const text = `<div id="note">
-  <div class="note_text">
-   ${note}
-   div class="note_btns">
-   <button class="note_edit_btn">🖋</button>
-   <button class="note_dlt_btn">🗑</button>
- </div>
- </div>`;
-  noteContainer.insertAdjacentHTML("afterbegin", text);
-};
